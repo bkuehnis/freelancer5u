538030018d496451ddc250624828be3715bfdc25
bkuehnis <62989491+bkuehnis@users.noreply.github.com>
2026-05-05 11:36:03 +0200

    first commit

---
+++ src/main/java/ch/zhaw/freelancer5u/Freelancer5uApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.freelancer5u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Freelancer5uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Freelancer5uApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/freelancer5u/Freelancer5uApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.freelancer5u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Freelancer5uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
